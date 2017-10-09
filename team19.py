dc77ffe795a77a2278e75c106db49124352deb2b
GalinaLina <32580117+GalinaLina@users.noreply.github.com>
2017-10-09 15:01:38 -0500

    change team19

--- team19.py
+++ team19.py
@@ -1,19 +1,16 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
-
-team_name = 'GoPsycho' # Only 10 chars displayed.
-strategy_name = 'Be nice at first, then always betray'
-strategy_description = 'Be nice at first, then always betray'
+team_name = 'IDK' # Only 10 chars displayed.
+strategy_name = 'The name the team gives to this strategy'
+strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
-    #always collude if it is before turn 100
-    if len(my_history) < 80:
-        return 'c'
-    #always betray starting at turn 80
+    if len(my_history) <= 11:
+        return "c"
+    elif "b" in their_history[0:11]:
+        return "b"
+    
     else:
-    	return 'b'
+        return "c"
+    
+    
+
+    
